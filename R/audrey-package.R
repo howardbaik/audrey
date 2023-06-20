7b7bfddad8f5d76f0da39d9988aa948b1f8774cf
howardbaek <howardbaek.fh@gmail.com>
2023-06-19 21:59:23 -0700

    Add Imports

--- R/audrey-package.R
+++ R/audrey-package.R
@@ -4,5 +4,7 @@
 ## usethis namespace: start
 #' @importFrom cli cli_alert_danger
 #' @importFrom cli cli_alert_success
+#' @importFrom tools file_path_sans_ext
+#' @importFrom withr with_path
 ## usethis namespace: end
 NULL
